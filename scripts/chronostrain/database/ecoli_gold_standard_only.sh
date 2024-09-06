92c612005d3919f070c57793b52ec584e40cee9c
Younhun Kim <ykim78@bwh.harvard.edu>
2024-09-06 11:53:59 -0400

    Added MISC_DATA_DIR pathing to point directly to marker seed index. moved INI files to `script/chronostrain/` directory.

--- scripts/chronostrain/database/ecoli_gold_standard_only.sh
+++ scripts/chronostrain/database/ecoli_gold_standard_only.sh
@@ -4,10 +4,15 @@ source settings_global.sh
 # Required Files
 CHRONOSTRAIN_DB_DIR="${DATA_DIR}/inference/databases/chronostrain/ecoli"
 GOLD_STANDARD_INDEX="${DATA_DIR}/gold_standard_genomes/index.tsv"
-MARKER_SEED_INDEX=../../../data/seeds/ecoli/marker_seed_index.tsv
+
+MARKER_SEED_INDEX="${MISC_DATA_DIR}/chronostrain_seeds/ecoli/marker_seed_index.tsv"
+if [ ! -f ${MARKER_SEED_INDEX} ]; then
+  echo "Extracting marker seeds from archive."
+  tar -xvzf "${MISC_DATA_DIR}/chronostrain_seeds.tar.gz" -C "${MISC_DATA_DIR}"
+fi
 
 # Reference Genome/Isolate BLAST database
-BLAST_DB_DIR=CHRONOSTRAIN_DB_DIR / "blast_db"  # The location of the Blast DB that you wish to create using RefSeq indices (they will be downloaded by this notebook).
+BLAST_DB_DIR="${CHRONOSTRAIN_DB_DIR}/blast_db"  # The location of the Blast DB that you wish to create using RefSeq indices (they will be downloaded by this notebook).
 BLAST_DB_NAME="CAMI_Strain_Madness_Gold"  # Blast DB to create.
 
 # chronostrain-specific settings
@@ -16,8 +21,8 @@ MARKER_MIN_PCT_IDTY=75  # accept BLAST hits as markers above this threshold
 CHRONOSTRAIN_TARGET_JSON="${CHRONOSTRAIN_DB_DIR}/gold_standard_only.json"
 
 # Some checks.
-if [ ! -f ${GOLD_STANDARD_INDEX} ]; then echo "Couldn't find ${GOLD_STANDARD_INDEX}"; fi
-if [ ! -f ${MARKER_SEED_INDEX} ]; then echo "Couldn't find ${MARKER_SEED_INDEX}"; fi
+if [ ! -f "${GOLD_STANDARD_INDEX}" ]; then echo "Couldn't find ${GOLD_STANDARD_INDEX}"; fi
+if [ ! -f "${MARKER_SEED_INDEX}" ]; then echo "Couldn't find ${MARKER_SEED_INDEX}"; fi
 mkdir -p "${CHRONOSTRAIN_DB_DIR}"
 mkdir -p "${BLAST_DB_DIR}"
 echo "All necessary files accounted for."
@@ -25,13 +30,13 @@ echo "All necessary files accounted for."
 # Construct the gold-standard-only DB.
 env \
     JAX_PLATFORM_NAME=cpu \
-    CHRONOSTRAIN_DB_DIR={CHRONOSTRAIN_DB_DIR} \
+    CHRONOSTRAIN_DB_DIR="${CHRONOSTRAIN_DB_DIR}" \
     CHRONOSTRAIN_LOG_INI=logging.ini \
     chronostrain -c chronostrain.ini \
       make-db \
-      -m $MARKER_SEED_INDEX \
-      -r $GOLD_STANDARD_INDEX \
-      -b $BLAST_DB_NAME -bd $BLAST_DB_DIR \
-      --min-pct-idty $MARKER_MIN_PCT_IDTY \
-      -o $CHRONOSTRAIN_TARGET_JSON \
-      --threads $NUM_CORES
+      -m "$MARKER_SEED_INDEX" \
+      -r "$GOLD_STANDARD_INDEX" \
+      -b $BLAST_DB_NAME -bd "$BLAST_DB_DIR" \
+      --min-pct-idty "$MARKER_MIN_PCT_IDTY" \
+      -o "$CHRONOSTRAIN_TARGET_JSON" \
+      --threads "$NUM_CORES"
--- scripts/settings_global.sh
+++ scripts/settings_global.sh
@@ -1,5 +1,9 @@
 export DATA_DIR="/mnt/e/CAMI_strain_madness"
 export SCRIPT_DIR=$(pwd)
+export MISC_DATA_DIR=${SCRIPT_DIR}/../data
 
 export GOLD_STANDARD_DIR=${DATA_DIR}/gold_standard_genomes
 export GOLD_STANDARD_GZ=${GOLD_STANDARD_DIR}/archives/strmgCAMI2_genomes.tar.gz
+
+export CHRONOSTRAIN_INI="chronostrain/chronostrain.ini"
+export CHRONOSTRAIN_LOG_INI="chronostrain/logging.ini"
