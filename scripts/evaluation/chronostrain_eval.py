0898120cc2d790d59edc5f71472fc5ea17cdef4e
Younhun Kim <ykim78@bwh.harvard.edu>
2024-09-06 20:27:43 -0400

    added straingst_eval script.

--- scripts/evaluation/chronostrain_eval.py
+++ scripts/evaluation/chronostrain_eval.py
@@ -43,6 +43,35 @@ class ChronoStrainInferenceResult:
         adhoc_rep_id = self.adhoc_clusters[strain_id]
         return self.inference_ordering[adhoc_rep_id], self.adhoc_clust_sizes[adhoc_rep_id]
 
+    def to_profile(
+            self,
+            strain_id_ordering: List[str],
+            posterior_lb: float = 0.9901,
+            n_samples: int = 5000
+    ) -> StrainAbundanceProfile:
+        g, z = self.get_samples(n_samples)
+
+        # Filter by posterior
+        posterior_p = self.posterior_inclusion_probs()
+        indicators = posterior_p > posterior_lb
+
+        log_indicators = np.empty(indicators.shape, dtype=float)
+        log_indicators[indicators] = 0.0
+        log_indicators[~indicators] = -np.inf
+
+        pred_abundances_wrapped = scipy.special.softmax(g + np.expand_dims(log_indicators, axis=[0, 1]), axis=-1)
+
+        # Fill in the prediction matrix. (Undo the ad-hoc clustering by dividing the abundance.)
+        predictions = np.empty(shape=(n_samples, len(strain_id_ordering)), dtype=float)
+        for tgt_idx, tgt_strain in enumerate(strain_id_ordering):
+            adhoc_idx, adhoc_clust_sz = self.adhoc_cluster_info(tgt_strain)
+            predictions[:, tgt_idx] = pred_abundances_wrapped[0, :, adhoc_idx] / adhoc_clust_sz
+
+        return StrainAbundanceProfile(
+            abundance_ratios=predictions / np.sum(predictions, axis=-1, keepdims=True),
+            strain_ids=strain_id_ordering
+        )
+
 
 def parse_chronostrain_strains(txt_file: Path) -> List[str]:
     with open(txt_file, 'rt') as f:
@@ -73,34 +102,4 @@ def extract_chronostrain_prediction(inference_dir: Path) -> ChronoStrainInferenc
         parse_posterior_model(inference_dir / f"posterior.{cfg.engine_cfg.dtype}.npz", len(strain_list)),
         strain_list,
         parse_adhoc_clusters(inference_dir / "adhoc_cluster.txt"),
-    )
-
-
-def chronostrain_prediction_to_profile(
-        inference_result: ChronoStrainInferenceResult,
-        strain_id_ordering: List[str],
-        posterior_lb: float = 0.9901,
-        n_samples: int = 5000
-) -> StrainAbundanceProfile:
-    g, z = inference_result.get_samples(n_samples)
-
-    # Filter by posterior
-    posterior_p = inference_result.posterior_inclusion_probs()
-    indicators = posterior_p > posterior_lb
-
-    log_indicators = np.empty(indicators.shape, dtype=float)
-    log_indicators[indicators] = 0.0
-    log_indicators[~indicators] = -np.inf
-    
-    pred_abundances_wrapped = scipy.special.softmax(g + np.expand_dims(log_indicators, axis=[0, 1]), axis=-1)
-
-    # Fill in the prediction matrix. (Undo the ad-hoc clustering by dividing the abundance.)
-    predictions = np.empty(shape=(n_samples, len(strain_id_ordering)), dtype=float)
-    for tgt_idx, tgt_strain in enumerate(strain_id_ordering):
-        adhoc_idx, adhoc_clust_sz = inference_result.adhoc_cluster_info(tgt_strain)
-        predictions[:, tgt_idx] = pred_abundances_wrapped[0, :, adhoc_idx] / adhoc_clust_sz
-
-    return StrainAbundanceProfile(
-        abundance_ratios=predictions / np.sum(predictions, axis=-1, keepdims=True),
-        strain_ids=strain_id_ordering
     )
---
+++ scripts/evaluation/straingst_eval.py
@@ -0,0 +1,34 @@
+from typing import *
+from pathlib import Path
+
+from numpy as np
+import pandas as pd
+from base import StrainAbundanceProfile
+
+
+class StrainGEInferenceResult:
+    def __init__(self, result_df: pd.DataFrame):
+        self.result_df = result_df
+
+    def to_profile(
+            self,
+            strain_id_ordering: List[str]
+    ):
+        predictions = np.empty(shape=(len(strain_id_ordering),), dtype=float)
+        for tgt_idx, tgt_strain in enumerate(strain_id_ordering):
+            strain_hit_rows = self.result_df.loc[self.result_df['strain'] == tgt_strain, 'rapct']
+            if strain_hit_rows.shape[0] > 1:
+                raise ValueError(f"Found more than one entry containing strain ID {tgt_strain}. Unable to resolve ambiguity.")
+            elif strain_hit_rows.shape[0] == 0:
+                predictions[tgt_idx] = 0.0
+            else:
+                predictions[tgt_idx] = strain_hit_rows.head(1).item() / 100.0
+        return StrainAbundanceProfile(
+            abundance_ratios=predictions / np.sum(predictions, axis=-1, keepdims=True),
+            strain_ids=strain_id_ordering
+        )
+
+
+def extract_straingst_prediction(inference_dir: Path) -> StrainGEInferenceResult:
+    tsv_file = inference_dir / "result.strains.tsv"
+    return StrainGEInferenceResult(pd.read_csv(tsv_file, sep='\t', index_col="i"))
