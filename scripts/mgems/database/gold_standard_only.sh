1cf66a4bff770107a2db5ef844223ff156e2e7c9
Younhun Kim <ykim78@bwh.harvard.edu>
2024-09-07 01:51:10 -0400

    added mgems scripts (db construction)

--- scripts/mgems/database/gold_standard_only.sh
+++ scripts/mgems/database/gold_standard_only.sh
@@ -10,5 +10,6 @@ cd "${MGEMS_GOLD_STANDARD_ONLY_REF_DIR}" || exit 1
 # Extract the list of fasta file paths.
 # sed | cut: skip the first line (header) and extract sixth column.
 sed '1d' "${GOLD_STANDARD_INDEX}" | cut -f6 > themisto_ref_paths.txt
+mkdir __tmp
 themisto build -k 31 -i themisto_ref_paths.txt -o themisto_index --temp-dir __tmp
 rm -rf __tmp
