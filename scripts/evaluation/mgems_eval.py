d98d968c20e4c43c083189b6ef2c537dabd4a615
Younhun Kim <ykim78@bwh.harvard.edu>
2024-09-09 10:51:36 -0400

    added mgems_eval.py - standardized gold_standard_only db construction for mgems.

---
+++ scripts/evaluation/mgems_eval.py
@@ -0,0 +1,53 @@
+from typing import *
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from base import StrainAbundanceProfile
+
+
+class MGEMSFlatInferenceResult:
+    def __init__(self, abund_df: pd.DataFrame):
+        self.abund_df = abund_df
+
+    def to_profile(
+            self,
+            strain_id_ordering: List[str]
+    ):
+        predictions = np.empty(shape=(len(strain_id_ordering),), dtype=float)
+        for tgt_idx, tgt_strain in enumerate(strain_id_ordering):
+            strain_hit_rows = self.abund_df.loc[self.abund_df['ID'] == tgt_strain, 'Abundance']
+            if strain_hit_rows.shape[0] != 1:
+                raise ValueError(f"Found more than one entry containing strain ID {tgt_strain}. Unable to resolve ambiguity.")
+            elif strain_hit_rows.shape[0] == 0:
+                raise ValueError(f"Unable to locate strain ID {tgt_strain} in mSWEEP output dataframe.")
+            else:
+                predictions[tgt_idx] = strain_hit_rows.head(1).item()
+        return StrainAbundanceProfile(
+            abundance_ratios=predictions / np.sum(predictions, axis=-1, keepdims=True),
+            strain_ids=strain_id_ordering
+        )
+
+
+def msweep_parse_abundances(msweep_output_path: Path, ref_info_path: Path) -> pd.DataFrame:
+    df_entries = []
+    with open(msweep_output_path, "rt") as f:
+        for line in f:
+            line = line.strip()
+            if f.startswith("#"):
+                continue
+            tokens = line.split("\t")
+            if len(tokens) != 2:
+                raise ValueError("Found {} tokens in msweep file {}, but expected 2.".format(
+                    len(tokens), msweep_output_path
+                ))
+            df_entries.append({"Cluster": tokens[0], "Abundance": float(tokens[1])})
+
+    cluster_df = pd.read_csv(ref_info_path, sep='\t')
+    return pd.DataFrame(df_entries).merge(cluster_df, on='Cluster', how='inner')
+
+
+def extract_mgems_prediction(inference_dir: Path, ref_info_path: Path) -> MGEMSFlatInferenceResult:
+    return MGEMSFlatInferenceResult(
+        msweep_parse_abundances(inference_dir / "msweep_abundances.txt", ref_info_path)
+    )
--- scripts/mgems/database/gold_standard_only.sh
+++ scripts/mgems/database/gold_standard_only.sh
@@ -8,9 +8,14 @@ cd "${MGEMS_GOLD_STANDARD_ONLY_REF_DIR}" || exit 1
 echo "Working in ${MGEMS_GOLD_STANDARD_ONLY_REF_DIR}"
 
 
+# Create a table translating msweep cluster index to gold-standard ID.
+echo -e "ID\tFasta\tCluster"
+sed '1d' "${GOLD_STANDARD_INDEX}" | cut -f3,6 | awk -v OFS='\t' '{print $1,$2,NR-1}' >> ref_info.tsv
+
+
 # Extract the list of fasta file paths.
 # sed | cut: skip the first line (header) and extract sixth column (fasta path).
-sed '1d' "${GOLD_STANDARD_INDEX}" | cut -f6 > themisto_ref_paths.txt
+sed '1d' ref_info.tsv | cut -f2 > themisto_ref_paths.txt
 n_genomes=$(wc -l themisto_ref_paths.txt | awk '{ print $1 }')
 if [ $n_genomes -ne $MGEMS_GOLD_STANDARD_ONLY_N_COLORS ]; then
   echo "# of gold-standard genomes found (${n_genomes}) does not match the expected value (${MGEMS_GOLD_STANDARD_ONLY_N_COLORS})"
@@ -25,8 +30,7 @@ rm -rf __tmp
 
 
 # Create mSWEEP aux file (list of cluster IDs --- one per gold standard. Not running poppunk here)
+sed '1d' ref_info.tsv | cut -f3 > "${MGEMS_GOLD_STANDARD_ONLY_REF_CLUSTER}"
 
-> "${MGEMS_GOLD_STANDARD_ONLY_REF_CLUSTER}"
-for (( i=0; i<${n_genomes}; i+=1 )); do
-    echo "$i" >> "${MGEMS_GOLD_STANDARD_ONLY_REF_CLUSTER}"
-done
+
+
